c8e87f20e811713eb31e6d111d5eb26190ef0872
shamalamm <shamy_pw4@hotmail.co.uk>
2023-03-27 17:13:34 +0100

    function to create p element added

---
+++ main.js
@@ -0,0 +1,12 @@
+let addToDo = document.getElementById('addToList');
+let itemContainer = document.getElementById('itemContainer');
+let inputField = document.getElementById('input')
+
+addToDo.addEventListener('click', addFunc)
+
+function addFunc(){
+ let para = document.createElement('p')
+ itemContainer.appendChild(para)
+}
+
+
